54bf20b5d92d35643b37424d961db9364024c4e5
Akerele, Tosin James <akerele.james1699@gmail.com>
2020-08-04 00:51:41 +0100

    Update colorchange.js

--- colorchange.js
+++ colorchange.js
@@ -1,18 +1,11 @@
-// function myFunction(){
-//     var str = document.getElementById('this').innerHTML;
-//     var pos = str.search('occurs');
-//     document.getElementById("this2").innerHTML=pos;
-// }
-
-
-console.log(y);
-
 function change(){
-    var colors=['yellow', 'red', 'blue', 'green', 'orange', 'purple', "#fff"];
-    var y=Math.floor(Math.random()*((colors.length-1) - 0 + 1)) + 0;//This helps to generate random numbers between 0 & 5;
+    var colors=['yellow', 'red', 'blue', 'green', 'orange', 'purple', "white", "#FF6347", "#FFA500", "#F0E68C", "#32CD32", "#9ACD32", "#00CED1", "#CD853F"];
+    var y=Math.floor(Math.random()*((colors.length-1) - 0 + 1)) + 0;//This helps to generate random numbers between 0 & the maximum length of the array;
     document.body.style.backgroundColor=colors[y];
+    if (document.body.style.backgroundColor=="white"){
+        document.getElementById("this").style.color="#000";
+    }
+    else {
+        document.getElementById("this").style.color="white";
+    }
 }
-// function colorChange(){
-//     var z=document.body.style;
-//     z.backgroundColor=colors[y];
-// }
